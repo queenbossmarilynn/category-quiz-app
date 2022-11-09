b39127382a4a623ed103da270b1446615213a49e
queenbossmarilynn <marilynnmelissaramirez@gmail.com>
2022-11-09 00:44:05 -0800

    added logic for dropdown selection for categories, has an input section for name, and difficulty levels with dropdown. Has an error alert if all selections are not filled in. Working on getting the start button to work to go to the quiz page next.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { Switch } from '@mui/material';
-import { BrowserRouter, Route } from 'react-router-dom';
+// import { Switch } from '@mui/material';
+import { useState } from "react";
+import {Routes, Route } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
@@ -8,25 +9,24 @@ import Quiz from "./Pages/Quiz/Quiz";
 import Result from "./Pages/Result/Result";
 
 function App() {
+  const [name, setName] = useState("");
+  const fetchQuestions = () => {};
+
   return (
-    <BrowserRouter>
+    <>
       <div className="app" style={{backgroundImage:"url('./purple-bg.png')"}}>
         <Header />
-
-        <Switch>
-          <Route path='/' exact>
-            <Home />
-          </Route>
-          <Route path='/quiz' exact>
-            <Quiz />
-          </Route>
-          <Route path='/result' exact>
-            <Result />
-          </Route>
-        </Switch>
+       
+        <Routes>
+          <Route path='/' exact element={<Home name={name} setName={setName} fetchQuestions={fetchQuestions}/>} />
+          <Route path='/quiz' exact element={<Quiz/>} />
+          <Route path='/result' exact element={<Result/>} />
+        </Routes>
+        
       </div>
+
       <Footer className="footer"/>
-    </BrowserRouter>
+    </>
   );
 }
 
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,24 +1,82 @@
-import { TextField } from '@mui/material';
-import React from 'react';
 import "./Home.css";
+import { Button, MenuItem, TextField } from '@mui/material';
+import Categories, {} from '../../Data/Categories';
+import React, { Component, useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import ErrorMessage, {} from "../../components/ErrorMessage/ErrorMessage";
+// import { withRouter } from 'react-router';
 
-const Home = () => {
+
+const Home = ({ name, setName, fetchQuestions }) => {
+
+  const [category, setCategory] = useState('');
+  const [difficulty, setDifficulty] = useState('');
+  const [error, setError] = useState(false);
+
+  const history = useNavigate();
+ 
+  // If any of the options come back empty or not filled it will return an Error
+  const handleSubmit = () => {
+    if (!category || !difficulty || !name) {
+      setError(true);
+      return;
+    } else {
+      setError(false);
+      fetchQuestions(category, difficulty);
+      history.push("/quiz");
+      // this.props StackOverflow Link: https://stackoverflow.com/questions/53712860/react-js-history-push-is-not-a-function-error-and-it-isnt-navigating-to-a-dif
+    }
+  };
+
+
   return (
     <div className='content'>
       <div className='settings'>
         <span style= {{ fontSize: 30 }}>Quiz Settings</span>
         <div className='settings_select'>
+          {error && <ErrorMessage>Please Fill All of the Fields</ErrorMessage>}
           <TextField 
             style={{ marginBottom: 25 }}
             label='Enter Your Name'
             variant='outlined'
+            onChange={(e) => setName(e.target.value)}
           />
 
           <TextField 
             select label='Select Category'
             variant='outlined'
             style={{ marginBottom: 30 }}
-          />
+            onChange={(e) => setCategory(e.target.value)}
+            value={category}
+          >
+            {Categories.map((cat) => (
+                <MenuItem key={cat.category} value={cat.value}>
+                  {cat.category}
+                </MenuItem>
+            ))}
+          </TextField>
+
+          <TextField
+              select
+              label='Select Difficulty'
+              variant='outlined'
+              style={{ marginBottom: 30}}
+              onChange={(e) => setDifficulty(e.target.value)}
+              value={difficulty}
+          >
+              <MenuItem key='Easy' value='easy'>Easy</MenuItem>
+              <MenuItem key='Medium' value='medium'>Medium</MenuItem>
+              <MenuItem key='Hard' value='hard'>Hard</MenuItem>
+          </TextField>
+
+          <Button 
+            variant='contained'
+            color='primary'
+            size='large'
+            onClick={handleSubmit}
+          >
+            Start Quiz
+          </Button>
         </div>
       </div>
 
@@ -27,4 +85,5 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Home;
+// export default withRouter(Home);
